95b3644559887b9e21fc0d54ecc20caca0a0aa9e
Sheik Mohamed Hisham <shisham@corp.broadsoft.com>
2017-03-04 16:56:11 +0000

    Added new REST resource - no RibbonLB changes needed

--- src/main/java/hish/spring/cloud/leaderboard/resource/LeaderBoardResource.java
+++ src/main/java/hish/spring/cloud/leaderboard/resource/LeaderBoardResource.java
@@ -3,6 +3,7 @@
  */
 package hish.spring.cloud.leaderboard.resource;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +19,17 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class LeaderBoardResource {
 	
+	@Value("${server.port}")
+	private int port;
+	
 	@RequestMapping(value = "/lifts", method = RequestMethod.POST)
 	public String recordLift(LiftRequest liftRequest) {
-		log.debug("Got a request {}", liftRequest);
+		log.info("Got a request at %d {}", port, liftRequest);
 		return "success";
+	}
+	
+	@RequestMapping(value = "/answerme")
+	public String answerMe() {
+		return String.format("Answered by app running on %d", port);
 	}
 }
